90c89b20896fc598ef41b2535c1003f98ab50250
danielxshi <danielshi96@gmail.com>
2022-01-08 17:07:23 -0800

    Build update

--- pages/about.js
+++ pages/about.js
@@ -1,4 +1,4 @@
-import SmallHeader from '../components/header/smallHeader';
+// import SmallHeader from '../components/header/smallHeader';
 import BranchHeader from '../components/Organisms/BranchHeader';
 import RegistrationModule from '../components/Organisms/RegistrationModule';
 import History from '../components/Organisms/About/History'
@@ -6,7 +6,7 @@ import History from '../components/Organisms/About/History'
 export default function Home() {
   return (
     <body>
-      <SmallHeader header={"about"} overline={"Overline"}/>
+      {/* <SmallHeader header={"about"} overline={"Overline"}/> */}
       <BranchHeader header={"Anniversary Year"} overline={"Celebrating 5 Years of PIVOT"} paragraph={"This year PIVOT celebrates its 5th year anniversary - and what an exciting 5 years it has been! Read below to find out more about PIVOT’s unique history."}/>
       <History/>
       <RegistrationModule header={"GET INVOLVED"} overline={"overline"}/>
