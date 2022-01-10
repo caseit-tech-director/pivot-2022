c4deda1f402a38b021acd1d2514111c114ebaf43
danielxshi <danielshi96@gmail.com>
2022-01-09 21:06:43 -0800

    Adding particles

--- components/Home/HomeBanner.js
+++ components/Home/HomeBanner.js
@@ -1,15 +1,23 @@
 import styles from '../../styles/modules/_header.module.scss';
 import MaxLayout from '../Layout/MaxLayout';
+import Particle from '../particle';
+
 export default function HomeBanner(props) {
   return (
     <div className="bg-color--primary">
       <MaxLayout className="center--banner">
-        <div className={styles['home--banner--text--wrapper']}>
-          <div className={styles['header--text']}>
-            <h1 className={styles['logo--heading']}>{props.header}</h1>
-            <div className="overline">{props.overline}</div>
+        <div className="outer-container">
+          <Particle/>
+          <div
+            id="home--banner--text--wrapper"
+            className={styles['home--banner--text--wrapper']}
+          >
+            <div className={styles['header--text']}>
+              <h1 className={styles['logo--heading']}>{props.header}</h1>
+              <div className="overline">{props.overline}</div>
+            </div>
+            <p>{props.paragraph}</p>
           </div>
-          <p>{props.paragraph}</p>
         </div>
       </MaxLayout>
     </div>
