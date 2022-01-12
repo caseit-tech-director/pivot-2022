68ce871b67e3593f8f2e3a2c945fda27a552237f
danielxshi <danielshi96@gmail.com>
2022-01-11 21:12:52 -0800

    Adding title

--- components/layout.js
+++ components/layout.js
@@ -9,6 +9,7 @@ export default function Layout({ children }) {
   return (
     <>
       <Head>
+        <title>PIVOT</title>
         <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
       <div className="nav--wrapper">
