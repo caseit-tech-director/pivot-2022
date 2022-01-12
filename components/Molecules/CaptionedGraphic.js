83b0f7a9ea6994bfd2b04a3742a7feb7ecd11074
danielxshi <danielshi96@gmail.com>
2022-01-11 18:29:47 -0800

    Adding media query for about page

--- components/Molecules/CaptionedGraphic.js
+++ components/Molecules/CaptionedGraphic.js
@@ -1,10 +1,11 @@
 import Image from "next/image"
+import style from '../../styles/modules/_captioned-graphic.module.scss';
 
 export default function CaptionedGraphic (props) {
     return(
         <div className="captioned--graphic--wrapper grid--gc--s1--e6">
             <Image objectFit="contain" className="margin--bottom--sm" src={props.image} width={props.width} height={props.height} alt={props.alt}/>
-            <div className="graphic--content--container items--flex--med--align">
+            <div className={[style['graphic--content--container'], style['items--flex--med--align']].join(' ')}>
                 <div className="graphic--content flex">
                     <h6 className="subtitle-2 text--bold">{props.subtitle1}: &nbsp;</h6>
                     <p className="body-2">{props.info1}</p>
