e03400f9e52fd858d14f82305dffb8351ab9f853
danielxshi <danielshi96@gmail.com>
2022-01-11 20:35:33 -0800

    Adding particles to large header

--- components/header/largeHeader.js
+++ components/header/largeHeader.js
@@ -1,17 +1,22 @@
 import styles from '../../styles/modules/_header.module.scss';
+import Particle from '../particle';
+
 
 function LargeHeader(props) {
   return (
-    <header className="max--layout text-color--white bg-color--primary">
-      <div className="center--banner max--layout grid--container">
-        <div className={styles['header--lrg--text--wrapper']}>
-          <div className={styles['header--content']}>
-            <div className="overline">{props.overline}</div>
-            <h1 className={styles['logo--heading']}>{props.header}</h1>
+    <div className="particle--contain">
+      <Particle/>
+      <header className="max--layout text-color--white bg-color--primary">
+        <div className="center--banner max--layout grid--container">
+          <div className={styles['header--lrg--text--wrapper']}>
+            <div className={styles['header--content']}>
+              <div className="overline">{props.overline}</div>
+              <h1 className={styles['logo--heading']}>{props.header}</h1>
+            </div>
           </div>
         </div>
-      </div>
-    </header>
+      </header>
+    </div>
   );
 }
 
