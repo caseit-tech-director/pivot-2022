48d061f6cdd5de1bfdf8a8da62c1f9fc5728d95d
danielxshi <danielshi96@gmail.com>
2022-01-12 22:00:50 -0800

    Updating file path

--- components/BannerComponents/SponsorshipBanner.js
+++ components/BannerComponents/SponsorshipBanner.js
@@ -3,7 +3,7 @@ import style from '../../styles/modules/_banner.module.scss';
 import BannerHeader from './BannerHeader';
 import ArrowButton from '../Molecules/ArrowButton';
 import Image from 'next/image';
-import accenture from '../../public/images/sponsors/accenture.jpg';
+import accenture from "../../public/images/sponsors/accenture.jpg";
 import cpa from '../../public/images/sponsors/cpa.jpg';
 import ey from '../../public/images/sponsors/ey.jpg';
 import thrive from '../../public/images/sponsors/thrive.jpg';
