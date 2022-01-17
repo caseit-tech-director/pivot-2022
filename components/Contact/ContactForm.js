c961ad45e1184fcf0b9fbb7ec7b15b44d9dd1000
danielxshi <danielshi96@gmail.com>
2022-01-17 04:04:45 -0800

    Styling contact form

--- components/Contact/ContactForm.js
+++ components/Contact/ContactForm.js
@@ -1,31 +1,53 @@
+import style from '../../styles/modules/_contact.module.scss';
+
 export default function ContactForm() {
   return (
-    <div className="container">
-      <h4>SEND A MESSAGE</h4>
+    <div className={style['container']}>
+      <h4 className={style['contact--form--title']}>SEND A MESSAGE</h4>
       <form action="action_page.php">
-        <label htmlFor="fname">First Name</label>
-        <input
-          type="text"
-          id="fname"
-          name="firstname"
-          placeholder="Your name.."
-        />
-        <label htmlFor="lname">Last Name</label>
-        <input
-          type="text"
-          id="lname"
-          name="lastname"
-          placeholder="Your last name.."
-        />
-        <label htmlFor="email">Email</label>
-        <input type="text" id="email" name="email" placeholder="Your email.." />
-        <label htmlFor="subject">Subject</label>
-        <textarea
-          id="subject"
-          name="subject"
-          placeholder="Write something.."
-        ></textarea>
-        <input type="submit" value="SEND MESSAGE" />
+        <div className={style['label--input--wrapper']}>
+          <label className={style['form--label']} htmlFor="fname">
+            FULL NAME
+          </label>
+          <input
+            className={style['form--input']}
+            type="text"
+            id="fname"
+            name="name"
+            placeholder="Your Full Name"
+          />
+        </div>
+        <div className={style['label--input--wrapper']}>
+          <label className={style['form--label']} htmlFor="email">
+            EMAIL ADDRESS
+          </label>
+          <input
+            className={style['form--input']}
+            type="text"
+            id="email"
+            name="email"
+            placeholder="Your Email Address"
+          />
+        </div>
+        <div className={style['label--input--wrapper']}>
+          <label className={style['form--label']} htmlFor="subject">
+            MESSAGE
+          </label>
+          <textarea
+            rows="4"
+            className={[
+              style['form--input'],
+              style['form--message--input'],
+            ].join('')}
+            id="subject"
+            name="subject"
+            placeholder="Message"
+          ></textarea>
+        </div>
+
+        <button type="submit" value="Your Message">
+          SEND MESSAGE
+        </button>
       </form>
     </div>
   );
