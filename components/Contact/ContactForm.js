9961db1884d11b46718018d968927d229dfdc62a
danielxshi <danielshi96@gmail.com>
2022-01-17 22:54:50 -0800

    Building endpoint for contact

--- components/Contact/ContactForm.js
+++ components/Contact/ContactForm.js
@@ -1,6 +1,41 @@
 import style from '../../styles/modules/_contact.module.scss';
+import { useState } from 'react';
 
 export default function ContactForm() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Sending');
+
+    let data = {
+      name,
+      email,
+      message,
+    };
+
+    fetch('/api/contact', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json, text/plain, */*',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    }).then((res) => {
+      console.log('Response received');
+      if (res.status === 200) {
+        console.log('Response succeeded!');
+        setSubmitted(true);
+        setName('');
+        setEmail('');
+        setBody('');
+      }
+    });
+  };
+
   return (
     <div className={style['container']}>
       <h4 className={style['contact--form--title']}>SEND A MESSAGE</h4>
@@ -15,6 +50,9 @@ export default function ContactForm() {
             id="fname"
             name="name"
             placeholder="Your Full Name"
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
           />
         </div>
         <div className={style['label--input--wrapper']}>
@@ -27,6 +65,9 @@ export default function ContactForm() {
             id="email"
             name="email"
             placeholder="Your Email Address"
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
           />
         </div>
         <div className={style['label--input--wrapper']}>
@@ -42,10 +83,13 @@ export default function ContactForm() {
             id="subject"
             name="subject"
             placeholder="Message"
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
           ></textarea>
         </div>
 
-        <button className="primary--btn" type="submit" value="Your Message">
+        <button className="primary--btn" type="submit" value="Your Message" onClick={(e)=>{handleSubmit(e)}}>
           SEND MESSAGE
         </button>
       </form>
