016cce7a2d4edc673057823812d8d70b6fa67bc2
danielxshi <danielshi96@gmail.com>
2022-01-18 13:18:56 -0800

    Testing success page

---
+++ components/Contact/success.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function success() {
+    return (
+        <View>
+            <Text>Thank you</Text>
+        </View>
+    )
+}
