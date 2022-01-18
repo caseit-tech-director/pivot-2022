c0c5a421613876f9aacac7ddf9da1d25511de0a5
danielxshi <danielshi96@gmail.com>
2022-01-18 15:59:44 -0800

    updating package.json build script

--- components/Contact/ContactForm.js
+++ components/Contact/ContactForm.js
@@ -71,27 +71,6 @@ export default function ContactForm() {
           <button type="submit">Send</button>
         </p>
       </form>
-
-      {/* <form
-    name="contact-form"
-    method="POST"
-    action="contact/?success=true"
-  >
-    <label htmlFor="name">Name *</label>
-    <input
-      id="name"
-      name="name"
-      required
-      type="text"
-    />
-    <label htmlFor="company">Company *</label>
-    <input id="company" name="company" required type="text" />
-    <label htmlFor="email">E-mail Address *</label>
-    <input id="email" type="email" name="email" required />
-    <label htmlFor="message">Message *</label>
-    <textarea id="message" name="message" required></textarea>
-    <button type="submit">Submit</button>
-  </form> */}
     </div>
   );
 }
