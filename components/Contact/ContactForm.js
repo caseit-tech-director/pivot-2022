569be3edb3a622436cc2c30dff5f6d68fdca2d3c
danielxshi <danielshi96@gmail.com>
2022-01-18 16:20:31 -0800

    Updating working contact form

--- components/Contact/ContactForm.js
+++ components/Contact/ContactForm.js
@@ -2,45 +2,46 @@ import style from '../../styles/modules/_contact.module.scss';
 import { useState, useEffect } from 'react';
 
 export default function ContactForm() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [submitted, setSubmitted] = useState(false);
+
   const [success, setSuccess] = useState(false);
   useEffect(() => {
     if (window.location.search.includes('success=true')) {
       setSuccess(true);
     }
   }, []);
-  // const [name, setName] = useState('');
-  // const [email, setEmail] = useState('');
-  // const [message, setMessage] = useState('');
-  // const [submitted, setSubmitted] = useState(false);
 
-  // const handleSubmit = (e) => {
-  //   e.preventDefault()
-  //   console.log('Sending')
+  const handleSubmit = (e) => {
+    // e.preventDefault();
+    console.log('Sending');
 
-  //   let data = {
-  //       name,
-  //       email,
-  //       message
-  //   }
+    let data = {
+      name,
+      email,
+      message,
+    };
 
-  //   fetch('../../pages/api/contact', {
-  //     method: 'POST',
-  //     headers: {
-  //       'Accept': 'application/json, text/plain, */*',
-  //       'Content-Type': 'application/json'
-  //     },
-  //     body: JSON.stringify(data)
-  //   }).then((res) => {
-  //       console.log('Response received')
-  //       if (res.status === 200) {
-  //           console.log('Response succeeded!')
-  //           setSubmitted(true)
-  //           setName('')
-  //           setEmail('')
-  //           setMessage('')
-  //       }
-  //   })
-  // }
+    fetch('api/contact', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json, text/plain, */*',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    }).then((res) => {
+      console.log('Response received');
+      if (res.status === 200) {
+        console.log('Response succeeded!');
+        setSubmitted(true);
+        setName('');
+        setEmail('');
+        setMessage('');
+      }
+    });
+  };
 
   return (
     <div className={style['container']}>
@@ -49,27 +50,70 @@ export default function ContactForm() {
         <p style={{ color: 'green' }}>Successfully submitted form!</p>
       )}
       <form
+        
         name="contact"
         method="POST"
         action="contact/?success=true"
         data-netlify="true"
       >
-        <input type="hidden" name="form-name" value="contact" />
-        <p>
-          <label htmlFor="name">Name</label>
-          <input type="text" id="name" name="name" />
-        </p>
-        <p>
-          <label htmlFor="email">Email</label>
-          <input type="text" id="email" name="email" />
-        </p>
-        <p>
-          <label htmlFor="message">Message</label>
-          <textarea id="message" name="message"></textarea>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
+        <div className={style['label--input--wrapper']}>
+          <label className={style['form--label']} htmlFor="name">
+            FULL NAME
+          </label>
+          <input
+            className={style['form--input']}
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
+        </div>
+        <div id={style['email']} className={style['label--input--wrapper']}>
+          <label className={style['form--label']} htmlFor="email">
+            EMAIL ADDRESS
+          </label>
+          <input
+            className={style['form--input']}
+            type="email"
+            name="email"
+            // pattern=".+@globex\.com"
+            placeholder="Your Email Address"
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+        </div>
+        <div className={style['label--input--wrapper']}>
+          <label className={style['form--label']} htmlFor="message">
+            MESSAGE
+          </label>
+          <textarea
+            rows="4"
+            className={[
+              style['form--input'],
+              style['form--message--input'],
+            ].join('')}
+            id="subject"
+            name="message"
+            placeholder="Message"
+            onChange={(e) => {
+              setMessage(e.target.value);
+            }}
+          ></textarea>
+        </div>
+
+        <button
+          className="primary--btn"
+          type="submit"
+          value="Your Message"
+          onClick={(e) => {
+            handleSubmit(e);
+          }}
+        >
+          SEND MESSAGE
+        </button>
       </form>
     </div>
   );
