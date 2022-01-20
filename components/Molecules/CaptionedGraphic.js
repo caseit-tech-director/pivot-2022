f33bf27072e4c9e3409809e5f8e4f053a0f29e42
danielxshi <danielshi96@gmail.com>
2022-01-19 23:18:43 -0800

    Aligning about pages text

--- components/Molecules/CaptionedGraphic.js
+++ components/Molecules/CaptionedGraphic.js
@@ -6,15 +6,15 @@ export default function CaptionedGraphic (props) {
         <div className="captioned--graphic--wrapper grid--gc--s1--e6">
             <Image objectFit="contain" className="margin--bottom--sm" src={props.image} width={props.width} height={props.height} alt={props.alt}/>
             <div className={[style['graphic--content--container'], style['items--flex--med--align']].join(' ')}>
-                <div className="graphic--content flex">
+                <div className={style['graphic--content']}>
                     <h6 className="subtitle-2 text--bold">{props.subtitle1}: &nbsp;</h6>
                     <p className="body-2">{props.info1}</p>
                 </div>
-                <div className="graphic--content flex">
+                <div className={style['graphic--content']}>
                     <h6 className="subtitle-2 text--bold">{props.subtitle2}: &nbsp;</h6>
                     <p className="body-2">{props.info2}</p>
                 </div>
-                <div className="graphic--content flex">
+                <div className={style['graphic--content']}>
                     <h6 className="subtitle-2 text--bold">{props.subtitle3}: &nbsp;</h6>
                     <p className="body-2">{props.info3}</p>
                 </div>
