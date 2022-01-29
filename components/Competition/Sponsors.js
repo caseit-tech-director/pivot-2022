3c40c88aa6430d7878fbd59da19b9a49d5ecccd3
danielxshi <danielshi96@gmail.com>
2022-01-29 14:37:19 -0800

    Adding media queries to competition

--- components/Competition/Sponsors.js
+++ components/Competition/Sponsors.js
@@ -1,7 +1,29 @@
 import ExtendableLayout from '../../components/Layout/ExtendableLayout';
 import style from '../../styles/modules/_sponsors.module.scss';
 import Image from 'next/image';
-import TestImg from '../../public/images/sponsors/accenture.jpg';
+
+// Title Sponsor(s)
+import Accenture from '../../public/images/sponsors/accenture.jpg';
+
+// Workshop Sponsor(s)
+import SalesForce from '../../public/images/sponsors/Salesforce.jpg';
+import Thrive from '../../public/images/sponsors/thrive.jpg';
+
+// Networking Sponsor(s)
+import CGI from '../../public/images/sponsors/network/CGI_logo_color_rgb.png';
+import CPA from '../../public/images/sponsors/network/cpa-bc-logo.png';
+import EY from '../../public/images/sponsors/network/ey-logo.png';
+import SAP from '../../public/images/sponsors/network/SAP-logo.png';
+import TMP from '../../public/images/sponsors/network/TMP-logo.png';
+
+// In Kind Sponsor(s)
+import EVO from '../../public/images/sponsors/in-kind/Evo_logo_TMRGB.jpg';
+import ScienceWorld from '../../public/images/sponsors/in-kind/ScienceWorld/1_Black/CMYK/SW_logo_richblack_cmyk.png';
+import StevesPoke from '../../public/images/sponsors/in-kind/steves-poke-bar.png';
+import StickerMule from '../../public/images/sponsors/in-kind/sticker-mule-logo-light-stacked.png';
+import MYST from '../../public/images/sponsors/in-kind/VAN_MYST_LOGO_FINALv5_HORIZONTAL LOGO600px.png';
+
+
 export default function Sponsors() {
   return (
     <>
@@ -17,7 +39,7 @@ export default function Sponsors() {
               <Image
                 width="200px"
                 height="200px"
-                src={TestImg}
+                src={Accenture}
                 alt="test img"
               />
             </div>
@@ -29,13 +51,14 @@ export default function Sponsors() {
                 <Image
                   width="200px"
                   height="200px"
-                  src={TestImg}
+                  layout="intrinsic"
+                  src={Thrive}
                   alt="test img"
                 />
                 <Image
                   width="200px"
                   height="200px"
-                  src={TestImg}
+                  src={SalesForce}
                   alt="test img"
                 />
               </div>
@@ -48,20 +71,43 @@ export default function Sponsors() {
                 <Image
                   width="200px"
                   height="200px"
-                  src={TestImg}
+                  src={EY}
+                  alt="test img"
+                  layout="intrinsic"
+                  objectFit='contain'
+
+                />
+                <Image
+                  width="200px"
+                  height="200px"
+                  layout="intrinsic"
+                  objectFit='contain'
+                  src={CGI}
                   alt="test img"
                 />
                 <Image
                   width="200px"
                   height="200px"
-                  src={TestImg}
+                  src={SAP}
                   alt="test img"
+                  layout="intrinsic"
+                  objectFit='contain'
                 />
                 <Image
                   width="200px"
                   height="200px"
-                  src={TestImg}
+                  src={CPA}
                   alt="test img"
+                  layout="intrinsic"
+                  objectFit='contain'
+                />
+                <Image
+                  width="200px"
+                  height="200px"
+                  src={TMP}
+                  alt="test img"
+                  layout="intrinsic"
+                  objectFit='contain'
                 />
               </div>
             </div>
@@ -72,26 +118,42 @@ export default function Sponsors() {
                 <Image
                   width="200px"
                   height="200px"
-                  src={TestImg}
+                  src={EVO}
                   alt="test img"
+                  layout="intrinsic"
+                  objectFit='contain'
                 />
                 <Image
                   width="200px"
                   height="200px"
-                  src={TestImg}
+                  src={ScienceWorld}
                   alt="test img"
+                  layout="intrinsic"
+                  objectFit='contain'
                 />
                 <Image
                   width="200px"
                   height="200px"
-                  src={TestImg}
+                  src={StevesPoke}
                   alt="test img"
+                  layout="intrinsic"
+                  objectFit='contain'
                 />
                 <Image
                   width="200px"
                   height="200px"
-                  src={TestImg}
+                  src={StickerMule}
                   alt="test img"
+                  layout="intrinsic"
+                  objectFit='contain'
+                />
+                <Image
+                  width="200px"
+                  height="200px"
+                  src={MYST}
+                  alt="test img"
+                  layout="intrinsic"
+                  objectFit='contain'
                 />
               </div>
             </div>
