32d561c673c6c91bd30e13cdf633400f2ebfadd4
danielxshi <danielshi96@gmail.com>
2022-02-04 05:20:31 -0800

    Aligning text on about page

--- components/Molecules/CaptionedGraphic.js
+++ components/Molecules/CaptionedGraphic.js
@@ -6,16 +6,16 @@ export default function CaptionedGraphic (props) {
         <div className="captioned--graphic--wrapper grid--gc--s1--e6">
             <Image objectFit="contain" className="margin--bottom--sm" src={props.image} width={props.width} height={props.height} alt={props.alt}/>
             <div className={[style['graphic--content--container'], style['items--flex--med--align']].join(' ')}>
-                <div className="graphic--content flex">
-                    <h6 className="subtitle-2 text--bold">{props.subtitle1}: &nbsp;</h6>
+                <div className="graphic--content flex--align">
+                    <h6 className="subtitle-2 text--bold">{props.subtitle1}:&#160; </h6>
                     <p className="body-2">{props.info1}</p>
                 </div>
-                <div className="graphic--content flex">
-                    <h6 className="subtitle-2 text--bold">{props.subtitle2}: &nbsp;</h6>
+                <div className="graphic--content flex--align">
+                    <h6 className="subtitle-2 text--bold">{props.subtitle2}:&#160; </h6>
                     <p className="body-2">{props.info2}</p>
                 </div>
-                <div className="graphic--content flex">
-                    <h6 className="subtitle-2 text--bold">{props.subtitle3}: &nbsp;</h6>
+                <div className="graphic--content flex--align">
+                    <h6 className="subtitle-2 text--bold">{props.subtitle3}:&#160; </h6>
                     <p className="body-2">{props.info3}</p>
                 </div>
             </div>
